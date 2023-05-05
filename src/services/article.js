4a013edcf3066f6ff9355c4525278e9479c3c63e
sofiatraba <sofiatrabam@gmail.com>
2023-05-05 11:07:48 +0200

    no-cors policy

--- src/services/article.js
+++ src/services/article.js
@@ -1,22 +1,29 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-export const articleApi = createApi({ 
-    reducerPath: 'articleApi',
-    baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
-            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
-            return headers;
-        }
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      return headers;
+    },
+    fetchOptions: {
+      mode: "no-cors",
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `/summarize/?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
-    endpoints: (builder) => ({
-        getSummary: builder.query({
-            query: (params) => `/summarize/?url=${encodeURIComponent(params.articleUrl)}&length=3`,
-        })
-    }),
+  }),
 });
 
 export const { useLazyGetSummaryQuery } = articleApi;
